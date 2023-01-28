1d44c5dc186091c42fd767c7172ddb4ed4951366
Minseo Cho <min1130@ewhain.net>
2023-01-28 21:09:25 +0900

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react'
+import './App.css';
+import Modal from './components/Modal';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      viewCompleted: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      },
+      todoList: []
+    };
+  }
+
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/tasks/")
+      .then(res => this.setState({ todoList: res.data }))
+      .catch(err => console.log(err))
+  };
+
+  // Create toggle property
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
+  handleSubmit = item => {
+    this.toggle();
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/tasks/${item.id}/`, item)
+        .then(res => this.refreshList())
+    }
+    axios
+      .post("http://localhost:8000/api/tasks/", item)
+      .then(res => this.refreshList())
+  };
+
+  handleDelete = item => {
+    axios
+      .delete(`http://localhost:8000/api/tasks/${item.id}/`)
+      .then(res => this.refreshList())
+  };
+
+  createItem = () => {
+    const item = { title: "", modal: !this.state.modal };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+
+  displayCompleted = status => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+    return this.setState({ viewCompleted: false });
+  };
+
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
+        >
+          Completed
+        </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}        
+        >
+          Incompleted
+        </span>
+      </div>
+    );
+  };
+
+  // Rendering items in the list (completed || incompleted)
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.todoList.filter(
+      item => item.completed === viewCompleted
+    );
+
+    return newItems.map(item => (
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+        
+        <span className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}
+          title={item.title}>
+          {item.title}
+        </span>
+        <span>
+          <button 
+            onClick={() => this.editItem(item)}
+            className="btn btn-info mr-2 m-2"
+          >
+              Edit
+          </button>
+          <button
+            onClick={() => this.handleDelete(item)} 
+            className="btn btn-danger mr-2"
+          >
+            Delete
+          </button>
+        </span>      
+      </li>
+    ))    
+  };
+
+  render() {
+    return (
+      <main className="content p-3 mb-2 bg-info">
+        <h1 className="text-white text-uppercase text-center my-4"> Task Manager </h1>
+        <div className="row">
+          <div className="col-md-6 col-sma-10 mx-auto p-0">
+            <div className="card p-3">
+              <div>
+                <button onClick={this.createItem} className="btn btn-warning">Add Task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <footer className="my-3 mb-2 bg-info text-white text-center">
+          Copyright 2023 &copy; All Rights Reserved
+        </footer>
+        {this.state.modal ? (
+          <Modal activeItem={this.state.activeItem} toggle={this.toggle} 
+          onSave={this.handleSubmit} />
+        ) : null}
+      </main>
+    )
+  }
+}
+
+export default App;
---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Input,
+    Label
+} from 'reactstrap';
+
+class CustomModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: this.props.activeItem
+        };
+    }
+
+    // To check if the checkbox is checked or not
+    handleChange = e => {
+        let { name, value } = e.target;
+        if (e.target.type === "checkbox") {
+            value = e.target.checked;
+        }
+        const activeItem = { ...this.state.activeItem, [name]: value };
+        this.setState({ activeItem });
+    };
+
+    render() {
+        const { toggle, onSave } = this.props;
+        return (
+            <Modal isOpen={true} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Task Item</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        {/* 1 title label */}
+                        <FormGroup>
+                            <Label for="title">Title</Label>
+                            <Input
+                                type="text"
+                                name="title"
+                                value={this.state.activeItem.title}
+                                onChange={this.handleChange}
+                                placeholder="Enter Task Title"
+                            />
+                        </FormGroup>
+
+                        {/* 2 description label */}
+                        <FormGroup>
+                            <Label for="description">Description</Label>
+                            <Input
+                                type="text"
+                                name="description"
+                                value={this.state.activeItem.description}
+                                onChange={this.handleChange}
+                                placeholder="Enter Task Description"
+                            />
+                        </FormGroup>
+
+                        {/* 3 completed label */}
+                        <FormGroup>
+                            <Label for="completed">
+                                <Input
+                                    type="checkbox"
+                                    name="completed"
+                                    checked={this.state.activeItem.completed}
+                                    onChange={this.handleChange}
+                                />
+                                Completed
+                            </Label>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+                        Save
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        )
+    }
+}
+
+export default CustomModal
